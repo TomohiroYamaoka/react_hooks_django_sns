e428d0ee7d5438dcd1a62878528fa8eab9652027
山岡智裕 <>
2021-10-29 18:25:53 +0900

    Userモデル

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.conf import settings
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('email is must')
+
+        # normalize_emailで入力されたemailを正規化(大文字で入ってきたら全部小文字に直したりする)する
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        # set_password関数でパッスワードをハッシュ化させる。
+        user.set_password(password)
+        # ユーザーの情報をsaveする
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=50, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        return self.email
