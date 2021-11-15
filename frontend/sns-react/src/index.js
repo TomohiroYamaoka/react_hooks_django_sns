97dea98331da574d50e7d256e2b0e6c63052bcc9
山岡智裕 <>
2021-11-15 16:54:11 +0900

    Routerの設定追加

--- frontend/sns-react/src/index.js
+++ frontend/sns-react/src/index.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Route, BrowserRouter } from "react-route-dom";
+import Login from "./components/Login";
+import { CookieProvider } from "react-cookie";
 
-ReactDOM.render(
+const routing = (
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <BrowserRouter>
+      <CookieProvider>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/profiles" component={App} />
+      </CookieProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
+
+ReactDOM.render(document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
